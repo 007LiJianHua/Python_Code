75b532973300d6e4f31e8d1340594a4df9d0ca89
李建华(立花泷) <lijianhua@uuzu.com>
2022-07-25 22:09:16 +0800

    生成多行外星人

--- Alien_Invation/alien_invasion.py
+++ Alien_Invation/alien_invasion.py
@@ -17,7 +17,7 @@ def run_game():
     ship  = Ship(screen,ai_settings)  #创建一个飞船
     bullets = Group()   #创建一个用于存储子弹的编组
     aliens = Group()    #创建一个存储外星人的编组
-    gf.create_fleet(ai_settings,screen,aliens)
+    gf.create_fleet(ai_settings,screen,aliens,ship=ship)
     alien = Alien(ai_settings=ai_settings,screen=screen)    #创建一个外星人实例
     while True:
         gf.check_events(ship=ship,ai_settings=ai_settings,screen=screen,bullets=bullets)   #为了代码整洁，将函数写在了gf中。#相应键盘和鼠标事件
--- Alien_Invation/game_functions.py
+++ Alien_Invation/game_functions.py
@@ -61,20 +61,28 @@ def get_number_aliens_x(ai_settings,alien_width):
     number_alien_x = int(available_space_x / (2* alien_width))
     return number_alien_x
 
-def create_alien(ai_settings,screen,aliens,alien_number):
+def create_alien(ai_settings,screen,aliens,alien_number,row_number):
     "创建一个外星人并将其放到当前行"
     alien = Alien(ai_settings=ai_settings,screen=screen)
     alien_width = alien.rect.width
     alien.x = alien_width + 2 * alien_width * alien_number
-    alien.rect.x = alien.x
+    alien.rect.x = alien.x      #外星人的x坐标
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number       #外星人的y坐标
     aliens.add(alien)
 
-def create_fleet(ai_settings,screen,aliens):
+def create_fleet(ai_settings,screen,aliens,ship):
     "创建外星人群"
     alien = Alien(ai_settings=ai_settings,screen=screen)
     number_aliens_x = get_number_aliens_x(ai_settings=ai_settings,alien_width=alien.rect.width)
+    number_rows = get_number_rows(ai_settings=ai_settings,ship_height=ship.rect.height,alien_height=alien.rect.height)
+    #创建外星人群
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            #创建第一个外星人并将其加入到当前行
+            create_alien(ai_settings=ai_settings,screen=screen,aliens=aliens,alien_number=alien_number,row_number=row_number)
 
-    #创建第一行外星人
-    for alien_number in range(number_aliens_x):
-        #创建第一个外星人并将其加入到当前行
-        create_alien(ai_settings=ai_settings,screen=screen,aliens=aliens,alien_number=alien_number)
+def get_number_rows(ai_settings,ship_height,alien_height):
+    avaliable_space_y = (ai_settings.screen_width - (3* alien_height) - ship_height)
+    number_rows = int(avaliable_space_y / (2*alien_height))
+    return number_rows
+
